4cfd0df5ecaefd4650a9e92e7e0bea70f3000109
Chris Snowden <cdsnowden@gmail.com>
2017-08-19 20:20:07 +0100

    cities and questions added

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Small game - get as close to the country as possible by clicking the map.
 
 ``` in progress ```
+
+### To-do:
+-   Add question interface
+-   Loop through questions
+-   Design work
+-   Success animations
--- js/find.js
+++ js/find.js
@@ -14,10 +14,58 @@ function debounce(func, wait, immediate) {
     };
 };
 
-
 document.onreadystatechange = function () {
-    // check reaady before allowing pin adds
+    // check ready before allowing pin adds
     if (document.readyState == 'complete') {
+
+/* settings area */
+
+        // city constructor
+        function city(top, left) {
+            this.top = top;
+            this.left = left;
+        }
+        // set city list
+        var cities = {
+            'London' : new city(169,660),
+            'New York' : new city(240,305),
+            'Los Angeles' : new city(270,110),
+            'Buenos Aires' : new city(660,380),
+            'Port Stanley' : new city(760,412)
+        };
+        // set city initial locations
+        function setCityLocs() {
+            $.each(cities, function(key, value){
+                var top = this.top,
+                left = this.left,
+                width = '1600',
+                height = '824';
+                value.topPos = ( $('#map').height() * (top / height) );
+                value.leftPos = ( $('#map').width() * (left / width) );
+                /* temp fn adding pins to help add cities
+                    var city = $('<div class="city-' + key + '"></div>');
+                    $('#map-box').append(city);
+                    $(city).css({
+                        top : value.topPos,
+                        left : value.leftPos
+                    })
+                */
+            });
+        }
+        setCityLocs();
+
+        // set questions!
+        var questions = new Array();
+        questions.push('London');
+        questions.push('New York');
+        questions.push('Los Angeles');
+        questions.push('Buenos Aires');
+        questions.push('Port Stanley');
+
+        window.currentQn = questions[0];
+        console.log(currentQn);
+
+/* action area */
 
         // controls for pin adding
         var itr = 1;
@@ -31,7 +79,8 @@ document.onreadystatechange = function () {
           });
           $(point).attr( 'top-percent' , ( parseInt($(point).css('top'), 10) / $('#map').height() ) );
           $(point).attr( 'left-percent' , ( parseInt($(point).css('left'), 10) / $('#map').width() ) );
-          listAdd(itr, [event.clientY, event.clientX]);
+          listAdd(itr, [e.clientY - this.getBoundingClientRect().top, e.clientX - this.getBoundingClientRect().left] );
+          checkAccuracy( window.currentQn, e.clientY - this.getBoundingClientRect().top, e.clientX - this.getBoundingClientRect().left );
         itr++;
         });
         function listAdd(itr, coords) {
@@ -42,8 +91,24 @@ document.onreadystatechange = function () {
           $('#pin-list').html(list);
         };
 
-        var reziseFn = debounce(function() {
+        function checkAccuracy(qn,t,l) {
+                function closeCheck(a,b) {
+                    if ( (a - b) < 50 && (a - b) > -50 ) {
+                        return true;
+                    }
+                }
+            if ( closeCheck(cities[qn].topPos,t) && closeCheck(cities[qn].leftPos,l) ) {
+                console.log('close');
+            }
+            else {
+                console.log('far');
+            }
+        }
 
+
+        // resize call
+        var reziseFn = debounce(function() {
+            // move pins loc
             var pinsAll = $('div[class*=pin-]');
             $.each(pinsAll, function(key, item){
                 $(this).css({
@@ -51,10 +116,12 @@ document.onreadystatechange = function () {
                     left: ( this.getAttribute('left-percent') * $('#map').width() ) + 'px'
                 })
             });
-
+            // update cities loc
+            setCityLocs();
         }, 100);
         window.addEventListener('resize', function(e) {
             reziseFn();
         });
+
     }
 }
